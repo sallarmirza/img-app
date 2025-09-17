50897df391d650cfc6a419144e4869d5aa4034d1
Salar Mirza <sallarmirza77@gmail.com>
2025-09-17 12:47:54 +0500

    Adding navbar

--- img-app/src/App.jsx
+++ img-app/src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
+import { Home } from "./pages/Home";
 
 function App() {
-
   return (
     <>
+    <Home/>
 
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ img-app/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {ResponsiveAppBar} from '../components/Nav'
+export const Home = () => {
+  return (
+    <div>
+      <ResponsiveAppBar/>
+      <main>
+        <h2>Image Generator</h2>
+      </main>
+    </div>
+  );
+};
